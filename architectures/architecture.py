16c0d08c6957508ef50fd6943da541879f2cf72c
AndrewBeers <andrew_beers@alumni.brown.edu>
2018-06-26 21:38:35 -0400

    init: You're up, James

---
+++ architectures/architecture.py
@@ -0,0 +1,22 @@
+
+class Architecture():
+
+    def __init__(self, name='Architecture Object', **kwargs):
+
+        self.name = name
+
+        return
+
+    def __str__(self):
+        
+        print(self.name)
+
+
+
+def run_test():
+
+    return
+
+
+if __name__ == '__main__':
+    run_test()
---
+++ genes/gene.py
@@ -0,0 +1,21 @@
+
+class Gene():
+
+    def __init__(self, name='Gene Object', **kwargs):
+
+        self.name = name
+
+        return
+
+    def __str__(self):
+        
+        print(self.name, 'Gene')
+
+
+def run_test():
+
+    return
+
+
+if __name__ == '__main__':
+    run_test()
